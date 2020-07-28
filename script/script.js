482e073bb79450ba26ef14720192f69dcc3839f2
Andrey Kalugin <k4lugin.andrew@yandex.ru>
2020-07-28 10:46:07 +0300

    trash added

--- script/script.js
+++ script/script.js
@@ -1,8 +1,24 @@
 let buttonEdit = document.querySelector('.profile__button-edit');
-let buttonClose = document.querySelector('.popup__close');
+let popupClose = document.querySelector('.popup__button-close');
+
 let overlay = document.querySelector('.popup__overlay');
 let popup = document.getElementById('popup');
-let popupClose = document.querySelector('.popup__button-close');
+
+//ПОПАП ОБЩИЙ
+function openPopup(popupElement) {
+  // ...
+}
+
+// эту функцию можно переиспользовать для разных попапов
+
+editPopupButton.addEventListener('click', function () {
+  openPopup(editPopup); // открываем попап редактирования
+});
+
+newItemPopupButton.addEventListener('click', function () {
+  openPopup(newItemPopup); // открываем попап добавления
+});
+//ПОПАП ОБЩИЙ КОНЕЦ
 
 //ФУНКЦИЯ ДЛЯ КНОПКИ РЕДАКТИРОВАНИЯ
 
@@ -55,3 +71,60 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+//Кнопка добавления карточки
+let popupCard = document.getElementById('popup__card');
+let buttonAdd = document.querySelector('.profile__button-add');
+let popupCloseAdd = document.querySelector('.button-add');
+let addSubmit = document.querySelector('.button-add-submit');
+
+function showClickAdd() {
+    popupCard.classList.add('popup_active');  //присваивание дополнительного класса
+}
+
+buttonAdd.addEventListener('click', showClickAdd);  //запуск функции
+
+function closeClickAdd() {
+    popupCard.classList.remove('popup_active');  //удаление дополнительного класса
+}
+
+popupCloseAdd.addEventListener('click', closeClickAdd);  //запуск функции
+
+
+
+//ДОБАВЛЕНИЕ КАРТОЧЕК НА СТРАНИЦУ 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  
+
+
+
+
+  // ФУНКЦИЯ ЛАЙК
